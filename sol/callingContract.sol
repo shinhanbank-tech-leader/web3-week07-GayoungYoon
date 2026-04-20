8d6209e6074d118f97a5916710e8ae2e1c6a47b3
GayoungYoon <42773460+GayoungYoon@users.noreply.github.com>
2026-04-20 22:06:35 +0900

    Create callingContract.sol

---
+++ sol/callingContract.sol
@@ -0,0 +1,37 @@
+// ArbitraryAlert
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+    function relay(address hero, bytes memory data) external {
+        // send all of the data as calldata to the hero
+        (bool success, ) = hero.call(data);
+    }
+}
+
+//CallFunction
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IHero {
+    function alert() external;
+}
+
+contract Sidekick {
+    function sendAlert(address hero) external {
+        // TODO: alert the hero using the IHero interface
+        IHero(hero).alert();
+    }
+}
+
+//fallback
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+    function makeContact(address hero) external {
+        // TODO: trigger the hero's fallback function!
+        (bool success, ) = hero.call(abi.encodeWithSignature("afjl()"));
+    }
+
+}
